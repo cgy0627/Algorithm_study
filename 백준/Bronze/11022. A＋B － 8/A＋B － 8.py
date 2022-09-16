791c5fc62a12e774a976b552ee551514f9f88b04
gy <55884436+cgy0627@users.noreply.github.com>
2022-09-16 16:34:52 +0900

    [Bronze V] Title: A+B - 8, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.py
@@ -0,0 +1,8 @@
+#각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+import sys
+
+n = int(input())
+
+for i in range(1, n + 1):
+    a,b = map(int, sys.stdin.readline().strip().split())
+    print(f'Case #{i}: {a} + {b} = {a + b}')
