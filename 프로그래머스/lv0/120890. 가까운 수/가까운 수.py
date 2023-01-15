274afe104ec321185b37ca9c9529461bad39aed7
gy <55884436+cgy0627@users.noreply.github.com>
2023-01-15 18:26:09 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.py
@@ -1,10 +1,12 @@
 def solution(array, n):
-    array.sort()
-    for i in range(len(array)):
-        if array[i] > n:
-            if (array[i] - n) < (n - array[i-1]):
-                return array[i]
-            else:
-                return array[i-1]
+    return sorted(array, key=lambda x:(abs(x-n),x))[0]
+
+#     array.sort()
+#     for i in range(len(array)):
+#         if array[i] > n:
+#             if (array[i] - n) < (n - array[i-1]):
+#                 return array[i]
+#             else:
+#                 return array[i-1]
     
-    return array[-1]
+#     return array[-1]
