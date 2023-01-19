3d35e140dc7d692acccc57af3610bbccc2858d1d
gy <55884436+cgy0627@users.noreply.github.com>
2023-01-20 00:01:42 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.py
@@ -1,14 +1,14 @@
 def solution(my_string):
-    addition = my_string.split(" + ")
-    ans = 0
-    for phrase in addition:
-        if "-" in phrase:
-            subtraction = phrase.split(" - ")
-            a = int(subtraction[0])
-            for i in range(1, len(subtraction)):
-                a -= int(subtraction[i])
-            ans += a
-        else:
-            ans += int(phrase)
-    return ans
-                
+    return eval(my_string)
+#     addition = my_string.split(" + ")
+#     ans = 0
+#     for phrase in addition:
+#         if "-" in phrase:
+#             subtraction = phrase.split(" - ")
+#             a = int(subtraction[0])
+#             for i in range(1, len(subtraction)):
+#                 a -= int(subtraction[i])
+#             ans += a
+#         else:
+#             ans += int(phrase)
+#     return ans
