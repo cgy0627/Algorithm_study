10a89e0208c19239bf62c118b34857c5ea72d35c
chaenny <55884436+cgy0627@users.noreply.github.com>
2023-01-22 16:37:22 +0900

    [level 2] Title: 올바른 괄호, Time: 18.33 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,12 @@
+def solution(s):
+    stack = [s[0]]
+    for i in range(1, len(s)):
+        if len(stack) > 0:
+            if (s[i] == ")") & (stack[-1] == "("):
+                stack.pop()
+            else:
+                stack.append(s[i])
+        else:
+            stack.append(s[i])
+            
+    return stack == []
