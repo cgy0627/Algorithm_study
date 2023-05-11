537bedb10558af91dbbdef2c509f907cbbdad895
chaenny <55884436+cgy0627@users.noreply.github.com>
2023-05-11 13:11:57 +0900

    [Bronze I] Title: 최소공배수, Time: 132 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,18 @@
+def gcd(pair):
+    a,b = min(pair), max(pair)
+
+    if a == 0 or b == 1:
+        return b
+    
+    if b % a == 0:
+        return a
+    
+    while b % a != 0:
+        a,b = b%a, a
+    return a
+    
+for i in range(int(input())):
+    a,b = map(int, input().split())
+    ab_gcd = gcd([a,b])
+    
+    print(a*b//ab_gcd)
