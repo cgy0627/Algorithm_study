0fbabc85464e07f37ab77a7de8b6938e581ade27
chaenny <55884436+cgy0627@users.noreply.github.com>
2023-05-11 15:01:45 +0900

    [Silver IV] Title: 최대 GCD, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9417. 최대 GCD/최대 GCD.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+def gcd(pair):
+    a,b = min(pair), max(pair)
+
+    if a == 0 or b == 1:
+        return b
+    
+    if b % a == 0:
+        return a
+    
+    while b % a != 0:
+        a,b = b%a, a
+    return a
+
+for i in range(int(input())):
+    nums = list(map(int, input().split()))
+    combs = list(combinations(nums, 2))
+    print(sorted(list(map(gcd, combs)))[-1])
