2261b1e5b0d8515e1bb267e6fa038aadd63f3743
chaenny <55884436+cgy0627@users.noreply.github.com>
2023-06-17 00:52:13 +0900

    [unrated] Title: 배열의 원소 삭제하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,7 +1,4 @@
 def solution(arr, delete_list):
     delete_key = dict(zip(delete_list, delete_list))
-    ans = []
-    for num in arr:
-        if num not in delete_key:
-            ans.append(num)
-    return ans
+    
+    return [num for num in arr if num not in delete_key ]
